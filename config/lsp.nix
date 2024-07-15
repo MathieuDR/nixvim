f7c27ae6728af11f78f0464c9ceeb5d014eb8187
mathieu <mathieu@deraedt.dev>
2024-07-15 11:28:43 +0200

    🐛 Put enable in treesitter settings

--- config/lsp.nix
+++ config/lsp.nix
@@ -3,8 +3,8 @@
     plugins = {
       ## Treesitter
       treesitter = {
-        enable = true;
         settings = {
+          enable = true;
           ensureInstalled = ["bash" "c_sharp" "cmake" "css" "csv" "dart" "diff" "dockerfile" "eex" "elixir" "erlang" "gitignore" "heex" "http" "hurl" "html" "ini" "javascript" "jg" "json" "lua" "nix" "regex" "ruby" "scss" "terraform" "xml" "yaml" "markdown" "markdown_inline"];
           indent.enable = true;
           incremental_selection = {
